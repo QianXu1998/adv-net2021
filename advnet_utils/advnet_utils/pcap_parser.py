b66ebb4a30c5d533370235cf439244d66ac6e694
edgar-costa <cedgar@ethz.ch>
2021-12-02 14:24:45 +0000

    MAke sure that unexpected packets do not crash the pcap parser for waypoints

--- advnet_utils/advnet_utils/pcap_parser.py
+++ advnet_utils/advnet_utils/pcap_parser.py
@@ -1,11 +1,11 @@
 from scapy.all import RawPcapReader, DLT_EN10MB, DLT_RAW_ALT, DLT_PPP
 import struct
-import socket 
+import socket
 
 IP_PROTOCOL = 0x0800
 MPLS_PROTOCOL = 0x08847
 
-#constants
+# constants
 ETH_LEN = 14
 IP_LEN = 20
 # We cant assume after IP there is a transport layer
@@ -19,8 +19,10 @@ int_to_protocol = {
     17: "udp"
 }
 
+
 def int2ip(addr):
     return socket.inet_ntoa(struct.pack("!I", addr))
+
 
 def find_payload_offset(packet, zeros=32, data_offset=UDP_DATA_OFFSET):
     """Finds payload index"""
@@ -28,11 +30,12 @@ def find_payload_offset(packet, zeros=32, data_offset=UDP_DATA_OFFSET):
     for i, _byte in enumerate(packet):
         if _byte == 0:
             consecutive_zeros += 1
-        else: 
+        else:
             consecutive_zeros = 0
         if consecutive_zeros >= zeros:
             return i - zeros - data_offset + 1
-    return -1 # not found
+    return -1  # not found
+
 
 def pcap_to_flows_sequences(pcap_file):
     """Parses all flows and sequence numbers"""
@@ -65,26 +68,28 @@ def pcap_to_flows_sequences(pcap_file):
                 continue
             total_size = meta.wirelen
             # remove first layer (in theory ethernet)
-            ethertype = struct.unpack("!H", packet[12:default_packet_offset])[0]
+            ethertype = struct.unpack(
+                "!H", packet[12:default_packet_offset])[0]
 
             # only accept IP and MPLS
             if ethertype != IP_PROTOCOL and ethertype != MPLS_PROTOCOL:
                 continue
-            
+
             # remove ethernet
             packet = packet[default_packet_offset:]
 
-            # remove all mpls labels 
+            # remove all mpls labels
             if ethertype == MPLS_PROTOCOL:
                 lb0, lb1, lb2, mpls_ttl = struct.unpack("!BBBB", packet[:4])
                 bottom_of_stack = 0x1 & lb2
                 packet = packet[4:]
                 while bottom_of_stack != 1:
-                    lb0, lb1, lb2, mpls_ttl = struct.unpack("!BBBB", packet[:4])
+                    lb0, lb1, lb2, mpls_ttl = struct.unpack(
+                        "!BBBB", packet[: 4])
                     bottom_of_stack = 0x1 & lb2
                     packet = packet[4:]
 
-            #IP LAYER Parsing
+            # IP LAYER Parsing
             version = packet[0]
             ip_version = version >> 4
             # we only accept ipv4
@@ -101,15 +106,19 @@ def pcap_to_flows_sequences(pcap_file):
             # right now our payloads have something in
             # common: they are full of 0s. We assume we found
             # the payload when we find 50 consecutive 0s.
-            offset = find_payload_offset(packet)
-            payload = packet[offset:offset+UDP_DATA_OFFSET]
-            seq, proto, sport, dport = struct.unpack("!QBHH", payload)
+            try:
+                offset = find_payload_offset(packet)
+                payload = packet[offset:offset+UDP_DATA_OFFSET]
+                seq, proto, sport, dport = struct.unpack("!QBHH", payload)
+            # wrong parsing just ignore this packet
+            except:
+                continue
 
             # we do not use this anymore since it could come with a custom header from students
             ip_proto = packet[9]
 
             # for now only parse udp packets
-            if proto != 17: # udp
+            if proto != 17:  # udp
                 continue
 
             # build five tuple
@@ -120,6 +129,3 @@ def pcap_to_flows_sequences(pcap_file):
             flow_to_sequences.setdefault(five_tuple, set()).add(seq)
 
     return flow_to_sequences
-
-
-
